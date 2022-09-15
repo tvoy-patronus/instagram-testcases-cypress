750f72a77cadaa714c3934d1fc26f77fd277137c
tvoy-patronus <taispatronus@gmail.com>
2022-09-15 21:02:31 +0200

    css selectors are added to the code

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -7,20 +7,22 @@ describe('Login Form', () =>
     it('Login form displaying', () =>
     {
       cy.get('button').contains('Allow essential and optional cookies').click()
-      cy.contains('Phone number, username, or email').next().should('be.visible')
-      cy.contains('Password').next().should('be.visible')
+      cy.get('img[alt="Instagram"]').should('be.visible')
+      cy.get('input[name="username"]').should('be.visible')
+      cy.get('input[name="password"]').should('be.visible')
       cy.get('button[type="submit"]').should('be.visible')
       cy.get('button').contains('Log in with Facebook').should('be.visible')
       cy.get('a').contains('Forgot password').should('be.visible')
     })
      it('ability to log in', () =>
     {
-      cy.get('input[aria-label="Phone number, username, or email"]').type('tvoy_patronus')
-      cy.get('input[aria-label="Password"]').type('patronus1992')
+      cy.get('input[name="username"]').type('test.taia')
+      cy.get('input[name="password"]').type('testtaia1992')
       cy.get('button[type="submit"]').click()
       cy.get('svg[aria-label="Loading..."]').should('be.visible').should('exist')
       cy.get('svg[aria-label="Loading..."]').should('not.exist')
       cy.get('button').contains('Not now').click()
+      сy.get('a').contains('test.taia').should('be.visible').should('exist')
     })
     
 })
